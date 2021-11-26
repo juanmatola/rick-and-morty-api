6f6ff02f5fb1ca6c205b154de0e70b8e1c0d09c6
juanmatola <tolajuanmanuel@yahoo.com.ar>
2021-11-26 15:12:14 -0300

    Listo estilo y formulario de busqueda

--- js/main.js
+++ js/main.js
@@ -2,6 +2,6 @@ import showData from './modules/showData.js';
 
 let root = document.getElementById('root');
     
-fetch('https://rickandmortyapi.com/api/character/1,2,3,4,5,6,7,8')
+fetch('https://rickandmortyapi.com/api/character')
     .then(res => res.json())
-    .then(data => showData(data, root));
+    .then(data => showData(data.results, root));
